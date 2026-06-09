35e47fd5fe50f500a425d2761537350731a2d0f3
Helen <150084608+brandoline@users.noreply.github.com>
2026-06-08 22:02:40 -0300

    Novas alterações de lógica e aparência

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ let refeicao = document.getElementById('refeicao');
 let data = document.getElementById('data');
 let conviteDigital = document.getElementById('conviteDigital');
 
+let fazer = document.getElementById('fazer');
+let comer = document.getElementById('comer');
+let horario = document.getElementById('horario');
+
 function aceitarConvite(){
     decisao.classList.add("hidden");
     localidade.classList.remove("hidden");
@@ -18,18 +22,46 @@ function negarConvite(){
 function escolherRefeicao(escolha){
     localidade.classList.add("hidden");
     refeicao.classList.remove("hidden");
+
+    fazer.textContent = escolha;
 }
 
 function escolherData(escolha){
     refeicao.classList.add("hidden");
     data.classList.remove("hidden");
+
+    comer.textContent = escolha;
 }
 
 function marcarEncontro(){
+    let dataEncontro = document.getElementById("dataEncontro");
     data.classList.add("hidden");
     conviteDigital.classList.remove("hidden");
+    
+    dataMarcada = tratarDataHora(dataEncontro.value);
+    horario.textContent = dataMarcada;
+    conviteDigital.style.display = "flex";
+    conviteDigital.style.display.gap = "1rem";
 }
 
 function enviarLembrete(){
     alert('Marcado!');
+}
+
+function tratarDataHora(DataMarcada){
+    let data = new Date(DataMarcada); 
+
+    console.log('data: ' + data);
+    
+    let dia = data.getDay();
+    let mes = data.getMonth();
+    let ano = data.getFullYear();
+
+    console.log(ano);
+
+    let hora = data.getHours();
+    let minuto = data.getMinutes();
+
+    let dataMarcada = `${dia}/${mes}/${ano} ${hora}:${minuto}`;
+    return dataMarcada;
 }
